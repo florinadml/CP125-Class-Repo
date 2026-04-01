8da2bcd9e590b91f35636b6e1d5e5d5755913882
florinadml <florinapidilis@gmail.com>
2026-04-01 12:22:35 +0800

    ngfm

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -2,4 +2,17 @@ import pandas as pd
 
 
 def explore_data(filename):
-    pass
+    data = pd.read_csv(filename)
+    total_students = len(data)
+    subject = ["Math","Science","English"]
+    math_average = round(data["Math"].mean(), 1)
+    highest_math_student = data.loc(data["Math"].idmax(), "Name")
+
+    return {"total_students:", total_students,
+            "subject:", subject ,
+            "math_average:", math_average, 
+            "highest_math_student :", highest_math_student
+    }
+
+result = explore_data("data/students.csv")
+print(result)
--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -2,4 +2,10 @@ import pandas as pd
 
 
 def compare_averages(filename):
-    pass
+    data = pd.read_csv(filename)
+    subjects = ["Math", "Science", "English"]
+    math_mean =  round(data["Math"].mean(), 1)
+    science_mean =  round(data["Science"].mean(), 1)
+    english_mean =  round(data["English"].mean(), 1)
+
+    
